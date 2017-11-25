392ec55e8e9555e3ca5e8c13bff85248d4c15b54
Jerehme Gayle <jerehme.gayle@gmail.com>
2017-11-24 22:08:11 -0700

    Sign up view, form, and template created and working

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth import logout
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.views.generic import FormView
+from django.views.generic import FormView, CreateView
 
 from . import forms
 
+# Sends users to userdashboard apon login
 def dashboard(request):
     return render(request, 'users/dashboard.html')
 
+# Allows users to logout
 class LogoutView(LoginRequiredMixin, FormView):
     form_class = forms.LogoutForm
     template_name = 'users/logout.html'
@@ -17,3 +20,10 @@ class LogoutView(LoginRequiredMixin, FormView):
     def form_valid(self, form):
         logout(self.request)
         return HttpResponseRedirect(reverse('home'))
+
+# Allows users to sign up
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('users:dashboard')
+
